889efc3b6db12f9138d2a8aaead6f097e8c8998a
sanalvsmern <sanalvsmern@gmail.com>
2024-07-15 18:25:05 +0530

    updated

--- deepnetsoft-fe/src/Footer.js
+++ deepnetsoft-fe/src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <Container fluid>
             <Row className='footer'>
-                <Col md={12}>
+                <Col md={12} className='d-none d-lg-flex justify-content-center'>
                     <Row className='footerRow'>
                         <Col sm={12} md={12} lg={3} className='foot1'>
                             <Row>
@@ -16,46 +16,138 @@ function Footer() {
                                     <Row>
                                         <Col md={12} className='footFont2'>
                                             <Row className='d-flex justify-content-center'>
-                                                <Col md={1}>
-                                                    <img src={`${process.env.PUBLIC_URL}/images/tel1.png`} alt="tel1" />
+                                                <Col md={12} className='d-flex justify-content-center'>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/tel1.png`} alt="tel1" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;+1 470-788-8255</p>
+                                                    </span>
                                                 </Col>
-                                                <Col md={6} className='text-align-end'>
-                                                    <p>+1 470-788-8255</p>
-                                                </Col>
-                                            </Row>
-                                            <Row className='d-flex justify-content-center'>
-                                                <Col md={1}>
-                                                    <img src={`${process.env.PUBLIC_URL}/images/mail.png`} alt="mail" />
-                                                </Col>
-                                                <Col md={8}>
-                                                    <p>email@42barandgrill.com</p>
+                                                <Col md={12} className='d-flex justify-content-center'>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/mail.png`} alt="mail" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;email@42barandgrill.com</p>
+                                                    </span>
                                                 </Col>
                                             </Row>
-                                        </Col>
-                                        <Col md={12} className='footFont2'>
-
                                         </Col>
                                     </Row>
                                 </Col>
                             </Row>
                         </Col>
                         <Col sm={12} md={12} lg={3} className='foot1'>
-                            <Row></Row>
+                            <Row>
+                                <Col md={12} className='d-flex justify-content-center'>
+                                    <img src={`${process.env.PUBLIC_URL}/images/logo.png`} className='footLogo' alt="logo" />
+                                </Col>
+                                <Col md={12} className='footLogoTxt'>
+                                    <p className='footLogoTxt1'>DEEP&nbsp;</p>
+                                    <p className='footLogoTxt2'>NET&nbsp;</p>
+                                    <p className='footLogoTxt3'>SOFT</p>
+                                </Col>
+                                <Col md={12} className='d-flex justify-content-center'>
+                                    <img src={`${process.env.PUBLIC_URL}/images/twitgrp.png`} alt="twitgrp" />
+                                </Col>
+                            </Row>
                         </Col>
                         <Col sm={12} md={12} lg={3} className='foot1'>
                             <Row>
                                 <Col md={12} className='footFont1'>
-                                    <p>FIND US</p>
+                                    <p>Find us</p>
                                 </Col>
                                 <Col md={12}>
                                     <Row>
-                                        <Col md={12}></Col>
-                                        <Col md={12}></Col>
+                                        <Col md={12} className='footFont2'>
+                                            <Row className='d-flex justify-content-center'>
+                                                <Col md={12} className='d-flex justify-content-center' style={{ maxWidth: '200px' }}>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/drop.png`} alt="drop" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;327 Memorial Dr SE, Atlanta,
+                                                            GA 30312, USA</p>
+                                                    </span>
+                                                </Col>
+                                            </Row>
+                                        </Col>
                                     </Row>
                                 </Col>
                             </Row>
                         </Col>
                     </Row>
+                </Col>
+                <Col md={12} className='d-flex d-lg-none'>
+                    <Row className='footerRow d-flex d-lg-none'>
+                        <Col md={12} className='foot1 pb-3'>
+                            <Row>
+                                <Col md={12} className='d-flex justify-content-center'>
+                                    <img src={`${process.env.PUBLIC_URL}/images/logo.png`} className='footLogo' alt="logo" />
+                                </Col>
+                                <Col md={12} className='footLogoTxt'>
+                                    <p className='footLogoTxt1'>DEEP&nbsp;</p>
+                                    <p className='footLogoTxt2'>NET&nbsp;</p>
+                                    <p className='footLogoTxt3'>SOFT</p>
+                                </Col>
+                                <Col md={12} className='d-flex justify-content-center'>
+                                    <img src={`${process.env.PUBLIC_URL}/images/twitgrp.png`} alt="twitgrp" />
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col md={12} className='foot1'>
+                            <Row>
+                                <Col md={12} className='footFont1'>
+                                    <p>Connect with Us</p>
+                                </Col>
+                                <Col md={12}>
+                                    <Row>
+                                        <Col md={12} className='footFont2'>
+                                            <Row className='d-flex justify-content-center'>
+                                                <Col md={12} className='d-flex justify-content-center'>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/tel1.png`} alt="tel1" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;+1 470-788-8255</p>
+                                                    </span>
+                                                </Col>
+                                                <Col md={12} className='d-flex justify-content-center'>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/mail.png`} alt="mail" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;email@42barandgrill.com</p>
+                                                    </span>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col md={12} className='foot1'>
+                            <Row>
+                                <Col md={12} className='footFont1'>
+                                    <p>Find us</p>
+                                </Col>
+                                <Col md={12}>
+                                    <Row>
+                                        <Col md={12} className='footFont2'>
+                                            <Row className='d-flex justify-content-center'>
+                                                <Col md={12} className='d-flex justify-content-center' style={{ maxWidth: '200px' }}>
+                                                    <img style={{ width: '20px', height: '20px' }} src={`${process.env.PUBLIC_URL}/images/drop.png`} alt="drop" />
+                                                    <span>
+                                                        <p>&nbsp;&nbsp;327 Memorial Dr SE, Atlanta,
+                                                            GA 30312, USA</p>
+                                                    </span>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className='footCont'>
+                <Col sm={12} md={5} className='footCont1'>
+                    <p>© 2024 42 Bar & Grill. Developed by Deepnetsoft Solutions.</p>
+                </Col>
+                <Col sm={12} md={5} className='footCont1'>
+                    <p>Terms & Conditions&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy</p>
                 </Col>
             </Row>
         </Container>
